1ebaad91842e35ad007e50d333eedc9f3576d99c
Yaroslav Shtanko <81259415+Yaroslav1918@users.noreply.github.com>
2023-12-13 15:37:44 +0200

    Update app.ts

--- server/app.ts
+++ server/app.ts
@@ -13,7 +13,7 @@ import { routeNotFound } from "./middlewares/routeNotFound";
 import { authWithGoogle } from "./middlewares/authWithGoogle";
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
