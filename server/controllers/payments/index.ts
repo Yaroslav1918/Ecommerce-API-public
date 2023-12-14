b00249e97920ffd79d72658b6cc706fc8d3fe6b6
Yaroslav Shtanko <81259415+Yaroslav1918@users.noreply.github.com>
2023-12-14 18:34:51 +0200

    Update index.ts

--- server/controllers/payments/index.ts
+++ server/controllers/payments/index.ts
@@ -1,12 +1,10 @@
 import { getAllPayments } from "./getAllPayments";
 import { getAllPaymentsByUserID } from "./getAllPaymentsByUserID";
 import { getPayment } from "./getPayment";
-import { removePayment } from "./removePayment";
 import { stripePayment } from "./stripePayment";
 import { webhook } from "./webhook";
 
 export default {
-  removePayment,
   getPayment,
   getAllPayments,
   stripePayment,
